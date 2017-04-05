bcb172a41c8995cbc1a90fbde9f8d61b36819bc0
Chris Coralli <ccoralli@gmail.com>
2017-04-05 15:37:42 -0400

    allowed spotify auth headers.

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,7 +7,7 @@ const middleware = require('../middleware');
 module.exports = (app) => {
     app.use(function(req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, access-token");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, access-token, access_token");
         next();
     });
     app.get('/users', UserController.getUsers);
